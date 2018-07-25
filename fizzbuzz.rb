b9c44e10c6d8e69b4a74140ecb6033ef6be4ad4e
Kyla Ulibarri <k.barri.071@gmail.com>
2018-07-25 16:03:41 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,40 @@
+def fizzbuzz(number)
+  if number%3 
+    puts "fizz"
+  elsif number%5
+    puts "buzz"
+  elsif number%3 and number%5
+    puts "fizzbuzz"
+  else 
+    puts "   "
+  end
+end
+
+fizzbuzz(5)
+
+#def fizzbuzz(number)
+ # if number%5
+ #   puts "buzz"
+#  end
+#end
+
+#def fizzbuzz(number)
+ # if number%3 and number%5
+  #  puts "fizzbuzz"
+  #end
+#end
+
+
+
+
+
+
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when the number is divisable by 3" do
+    fizz_3 = fizzbuzz(3)
+    
+    expect (fizz_3).to
+eq("fizz")
+end
+it 
